d0742002a56e88460e5388176b1a6029e6a7d6f6
DimasAriel <dimasariel73@gmail.com>
2016-12-02 16:48:03 -0400

    Inmobi-Itla

--- inc/Functions/GetAssignedInterfaces.php
+++
@@ -1,7 +0,0 @@
-<?
-include $_SERVER['DOCUMENT_ROOT'] . "/inc/init.php";
-include $_SERVER['DOCUMENT_ROOT'] . "/inc/Repos/CompanyRepo.php";
-include $_SERVER['DOCUMENT_ROOT'] . "/inc/Repos/UserRepo.php";
-
-echo GetAssignedInterfaces($_SESSION['User']) ;
-?>
